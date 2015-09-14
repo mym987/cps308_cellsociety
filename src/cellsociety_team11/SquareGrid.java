4955d3b892f4ab604b827be5c67a19a860d98265
karenli95 <kjl32@duke.edu>
2015-09-14 18:30:21 -0400

    Updated GCL classes

--- src/cellsociety_team11/SquareGrid.java
+++ src/cellsociety_team11/SquareGrid.java
@@ -1,56 +1,42 @@
 package cellsociety_team11;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class SquareGrid extends Grid {
 
-	private double myWidth;
-	private double myHeight;
+	private double myWidthPixels;  //could move later
+	private double myHeightPixels;
 
-	public static int myNumRows = 5; // hardcoded and public rn
-	public static int myNumCols = 5; // hardcoded and public rn
+	Cell[][] myGrid;
 
-	GOLCell[][] myGrid;
 
-	public SquareGrid() {
-		super();
-		createGrid();
-		myWidth = myNumCols * GOLCell.CELL_SIZE;
-		myHeight = myNumRows * GOLCell.CELL_SIZE;
+	SquareGrid(int c, int r, Map<Location, Cell> cells) {
+		super(c, r, cells);
+		//createGrid();
+		myWidthPixels = myNumCols * GOLCell.CELL_SIZE;
+		myHeightPixels = myNumRows * GOLCell.CELL_SIZE;
 	}
 
-	public int getNumRows() {
-		return myNumRows;
-	}
-
-	public int getNumCols() {
-		return myNumCols;
-	}
-
-	protected void createGrid() {
-
-		Location loc = new Location(myHeight, myHeight);
-
-		// if (myWidth != this.getWidth() || myHeight != this.getHeight()) {
-		// this.setWidth(myWidth);
-		// this.setHeight(myHeight);
-
-		for (int x = 0; x < myNumRows; x++) {
-			for (int y = 0; y < myNumCols; y++) {
-				loc = new Location(x, y);
-				Cell cell = new GOLCell(loc);
-				myCells.add(cell);
-			}
-		}
-
-		// }
-	}
+//	protected void createGrid() {
+//
+//		Location loc = new Location(myNumCols, myNumRows);
+//
+//		for (int x = 0; x < myNumRows; x++) {
+//			for (int y = 0; y < myNumCols; y++) {
+//				loc = new Location(x, y);
+//				Cell cell = new GOLCell(loc);
+//				myCells.put(loc, cell);
+//			}
+//		}
+//
+//	}
 
 	public void setNeighbors() {
 
-		for (GOLCell[] cellArray : myGrid) {
-			for (GOLCell cell : cellArray) {
-				ArrayList<GOLCell> neighbors = new ArrayList<GOLCell>();
+		for (Cell[] cellArray : myGrid) {
+			for (Cell cell : cellArray) {
+				ArrayList<Cell> neighbors = new ArrayList<Cell>();
 				for (int i = -1; i < 2; i++) {
 					for (int j = -1; j < 2; j++) {
 						if (i != 0 || j != 0) {
