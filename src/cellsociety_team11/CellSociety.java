8d4a0a19adf6cc6fa8f5f69b74f14128c5aebb2a
Cameron Givler <cameron.givler@duke.edu>
2015-09-15 01:10:06 -0400

    window launches

--- src/cellsociety_team11/CellSociety.java
+++ src/cellsociety_team11/CellSociety.java
@@ -2,15 +2,25 @@ package cellsociety_team11;
 
 import gui.CellSocietyGUI;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class CellSociety extends Application {
- 
-    @Override public void start(Stage stage) throws Exception {
-    	new CellSocietyGUI(stage);
+    public static final int XSIZE = 1000;
+    public static final int YSIZE = 1000;
+    public static final int FRAMES_PER_SECOND = 60;
+    private static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+    private static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+   
+
+	@Override public void start(Stage stage) throws Exception {
+    	CellSocietyGUI myCSGUI = new CellSocietyGUI(stage);
+    	
+        Scene scene = myCSGUI.init(XSIZE, YSIZE);
+        stage.setScene(scene);
+        stage.show();
     }
- 
+
     public static void main(String[] args) {
         launch(args);
     }
--- src/gui/CellSocietyGUI.java
+++ src/gui/CellSocietyGUI.java
@@ -1,15 +1,28 @@
 package gui;
 
-import cellsociety_team11.Grid;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class CellSocietyGUI {
+	private static final String TITLE = "Cell Society";
+	
+	private Scene myScene;
+	private Group myRoot;
+
+	private int myWindowWidth, myWindowHeight;
 
 	public CellSocietyGUI(Stage stage) {
-        stage.setScene(new Scene(null));
-        stage.show();
 	}
+	
+	public Scene init(int width, int height) {
+		myWindowWidth = width;
+		myWindowHeight = height;
 
+		myRoot = new Group();
+		myScene = new Scene(myRoot, width, height, Color.WHITE);
+
+		return myScene;
+	}
 }
