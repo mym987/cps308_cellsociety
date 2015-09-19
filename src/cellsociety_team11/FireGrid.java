25382204c82429c819b37f3c03ea0e19f4c2066f
karenli95 <kjl32@duke.edu>
2015-09-18 21:57:35 -0400

    Updated Fire Simulation

---
+++ src/cellsociety_team11/FireGrid.java
@@ -0,0 +1,53 @@
+package cellsociety_team11;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class FireGrid extends SquareGrid{
+
+	FireGrid(int width, int height, Map<Location, Cell> cells) {
+		super(width, height, cells);
+	}
+	
+	public void setNeighbors() {
+		myCells.forEach((loc,cell)->{
+			cell.setNeighborCells(getAdjacentCells(cell));
+		});
+	}
+	
+	@Override
+	public List<Location> getAdjacentLoc(Location loc) {
+		int[] x = {-1, 0, 0, 1};
+		int[] y = {0, -1, 1, 0};
+		List<Location> list = new ArrayList<>(x.length);
+		for(int i=0;i<x.length;i++){
+			Location neighbor = loc.getLocation(loc.getX()+x[i], loc.getY()+y[i]);
+			if(neighbor!=null){
+				list.add(neighbor);
+			}
+		}
+		return list;
+	}
+
+	@Override
+	public List<Cell> getAdjacentCells(Cell cell) {
+		List<Cell> neighbors = new ArrayList<>(8);
+		getAdjacentLoc(cell.getLocation()).forEach(loc->{
+			neighbors.add(getCell(loc));
+		});
+		return neighbors;
+	}
+	
+	@Override
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+		for(int r=0;r<getHeight();r++){
+			for(int c=0;c<getWidth();c++){
+				//Cell cell = getCell(new Location(c,r,))
+			}
+		}
+		return "";
+	}
+
+}
