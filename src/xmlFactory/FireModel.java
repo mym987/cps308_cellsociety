fd066df636bccd1cb549ae2452703d2eba4c3097
Mike Ma <ym67@duke.edu>
2015-09-18 23:57:56 -0400

    Fix a bug that Segregation model displays nothing & added 2 XML

---
+++ src/xmlFactory/FireModel.java
@@ -0,0 +1,70 @@
+package xmlFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class FireModel extends Model {
+	
+	private static final int EMPTY_STATE = 0;
+	private static final int SHARK_STATE = 1;
+	private static final int FISH_STATE = 2;
+
+	private int myNumShark, myNumFish;
+
+	FireModel(int width, int height, double percentShark, double percentFish) {
+		super("PredModel", width, height);
+		int total = width * height;
+		percentShark = (percentShark < 0) ? 0 : percentShark;
+		percentFish = (percentFish < 0) ? 0 : percentFish;
+		int numSharks = (int) (percentShark * total);
+		int numFish = (int) (percentFish * total);
+		myNumShark = numSharks <= total ? numSharks : total;
+		myNumFish = (numFish <= total - myNumShark) ? numFish : total - myNumShark;
+	}
+
+	@Override
+	public List<Map<String, String>> getCells() {
+		int mat[][] = new int[getWidth()][getHeight()];
+		Random rand = new Random(System.currentTimeMillis());
+		int total = getWidth()*getHeight();
+		
+		int i = 0;
+		while(i < myNumShark){
+			int t = rand.nextInt(total);
+			int x = t % getWidth(), y = t / getWidth();
+			if(mat[x][y]==EMPTY_STATE){
+				mat[x][y] = SHARK_STATE;
+				i++;
+			}
+		}
+		i = 0;
+		while(i < myNumFish){
+			int t = rand.nextInt(total);
+			int x = t % getWidth(), y = t / getWidth();
+			if(mat[x][y]==EMPTY_STATE){
+				mat[x][y] = FISH_STATE;
+				i++;
+			}
+		}
+		List<Map<String, String>> list = new ArrayList<>();
+		
+		for (int x = 0; x < mat.length; x++) {
+			for (int y = 0; y < mat[x].length; y++) {
+				list.add(createCell(x, y, mat[x][y]));
+			}
+		}
+		return list;
+	}
+
+	private Map<String, String> createCell(int x, int y, int state) {
+		Map<String, String> map = new HashMap<>();
+		map.put("x", Integer.toString(x));
+		map.put("y", Integer.toString(y));
+		map.put("state", Integer.toString(state));
+		return map;
+	}
+
+}
--- src/xmlFactory/GOLModel.java
+++ src/xmlFactory/GOLModel.java
@@ -7,21 +7,39 @@ import java.util.Map;
 import java.util.Random;
 
 public class GOLModel extends Model {
+	
+	private final int myNumLiveCells;
+	private static final int DEAD_STATE = 0;
+	private static final int LIVE_STATE = 1;
 
-	private final double PROB_LIVE = 0.5;
-
-	GOLModel(int width, int height) {
+	GOLModel(int width, int height, double percentLive) {
 		super("GOLModel", width, height);
+		percentLive = (percentLive<0)?0:percentLive;
+		percentLive = (percentLive>1)?1:percentLive;
+		myNumLiveCells = (int)(width*height*percentLive);
 	}
 
 	@Override
 	public List<Map<String, String>> getCells() {
-		List<Map<String, String>> list = new ArrayList<>();
+		int mat[][] = new int[getWidth()][getHeight()];
 		Random rand = new Random(System.currentTimeMillis());
-		for (int x = 0; x < getWidth(); x++) {
-			for (int y = 0; y < getHeight(); y++) {
-				int state = rand.nextDouble() > PROB_LIVE ? 0 : 1;
-				list.add(createCell(x, y, state));
+		int total = getWidth()*getHeight();
+		
+		int i = 0;
+		while(i < myNumLiveCells){
+			int t = rand.nextInt(total);
+			int x = t % getWidth(), y = t / getWidth();
+			if(mat[x][y]==DEAD_STATE){
+				mat[x][y] = LIVE_STATE;
+				i++;
+			}
+		}
+		
+		List<Map<String, String>> list = new ArrayList<>();
+		
+		for (int x = 0; x < mat.length; x++) {
+			for (int y = 0; y < mat[x].length; y++) {
+				list.add(createCell(x, y, mat[x][y]));
 			}
 		}
 		return list;
